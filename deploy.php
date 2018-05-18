b2f3e5c6de5bd5402937c2aab10763ed1770dad1
technet-systems <wojcicki@technet.systems>
2018-05-18 11:09:27 +0200

    added deploy.php and config.php

--- deploy.php
+++ deploy.php
@@ -1,73 +1,87 @@
 <?php
-// Forked from https://gist.github.com/1809044
-// Available from https://gist.github.com/nichtich/5290675#file-deploy-php
-$TITLE   = 'Git Deployment Hamster';
-$VERSION = '0.11';
-echo <<<EOT
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>$TITLE</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
-  o-o    $TITLE
- /\\"/\   v$VERSION
-(`=*=') 
- ^---^`-.
-EOT;
-// Check whether client is allowed to trigger an update
-$allowed_ips = array(
-	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
-	'195.37.139.','193.174.' // VZG
-);
-$allowed = false;
-$headers = apache_request_headers();
-if (@$headers["X-Forwarded-For"]) {
-    $ips = explode(",",$headers["X-Forwarded-For"]);
-    $ip  = $ips[0];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
+require_once("config.php");
+$content = file_get_contents("php://input");
+$json    = json_decode($content, true);
+$file    = fopen(LOGFILE, "a");
+$time    = time();
+$token   = false;
+// retrieve the token
+if (!$token && isset($_SERVER["HTTP_X_HUB_SIGNATURE"])) {
+    list($algo, $token) = explode("=", $_SERVER["HTTP_X_HUB_SIGNATURE"], 2) + array("", "");
+} elseif (isset($_SERVER["HTTP_X_GITLAB_TOKEN"])) {
+    $token = $_SERVER["HTTP_X_GITLAB_TOKEN"];
+} elseif (isset($_GET["token"])) {
+    $token = $_GET["token"];
 }
-foreach ($allowed_ips as $allow) {
-    if (stripos($ip, $allow) !== false) {
-        $allowed = true;
-        break;
-    }
-}
-if (!$allowed) {
-	header('HTTP/1.1 403 Forbidden');
- 	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
-    echo "</pre>\n</body>\n</html>";
+// log the time
+date_default_timezone_set("UTC");
+fputs($file, date("d-m-Y (H:i:s)", $time) . "\n");
+// function to forbid access
+function forbid($file, $reason) {
+    // explain why
+    if ($reason) fputs($file, "=== ERROR: " . $reason . " ===\n");
+    fputs($file, "*** ACCESS DENIED ***" . "\n\n\n");
+    fclose($file);
+    // forbid
+    header("HTTP/1.0 403 Forbidden");
     exit;
 }
-flush();
-// Actually run the update
-$commands = array(
-	'echo $PWD',
-	'whoami',
-	'git pull',
-	'git status',
-	'git submodule sync',
-	'git submodule update',
-	'git submodule status',
-    'test -e /usr/share/update-notifier/notify-reboot-required && echo "system restart required"',
-);
-$output = "\n";
-$log = "####### ".date('Y-m-d H:i:s'). " #######\n";
-foreach($commands AS $command){
-    // Run it
-    $tmp = shell_exec("$command 2>&1");
-    // Output
-    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-    $output .= htmlentities(trim($tmp)) . "\n";
-    $log  .= "\$ $command\n".trim($tmp)."\n";
+// function to return OK
+function ok() {
+    ob_start();
+    header("HTTP/1.1 200 OK");
+    header("Connection: close");
+    header("Content-Length: " . ob_get_length());
+    ob_end_flush();
+    ob_flush();
+    flush();
 }
-$log .= "\n";
-file_put_contents ('deploy-log.txt',$log,FILE_APPEND);
-echo $output; 
-?>
-</pre>
-</body>
-</html>
+// Check for a GitHub signature
+if (!empty(TOKEN) && isset($_SERVER["HTTP_X_HUB_SIGNATURE"]) && $token !== hash_hmac($algo, $content, TOKEN)) {
+    forbid($file, "X-Hub-Signature does not match TOKEN");
+// Check for a GitLab token
+} elseif (!empty(TOKEN) && isset($_SERVER["HTTP_X_GITLAB_TOKEN"]) && $token !== TOKEN) {
+    forbid($file, "X-GitLab-Token does not match TOKEN");
+// Check for a $_GET token
+} elseif (!empty(TOKEN) && isset($_GET["token"]) && $token !== TOKEN) {
+    forbid($file, "\$_GET[\"token\"] does not match TOKEN");
+// if none of the above match, but a token exists, exit
+} elseif (!empty(TOKEN) && !isset($_SERVER["HTTP_X_HUB_SIGNATURE"]) && !isset($_SERVER["HTTP_X_GITLAB_TOKEN"]) && !isset($_GET["token"])) {
+    forbid($file, "No token detected");
+} else {
+    // check if pushed branch matches branch specified in config
+    if ($json["ref"] === BRANCH) {
+        fputs($file, $content . PHP_EOL);
+        // ensure directory is a repository
+        if (file_exists(DIR . ".git") && is_dir(DIR)) {
+            try {
+                // pull
+                fputs($file, "*** AUTO PULL INITIATED ***" . "\n");
+                chdir(DIR);
+                $result = shell_exec(GIT . " pull 2>&1");
+                fputs($file, $result . "\n");
+                // return OK to prevent timeouts on AFTER_PULL
+                ok();
+                // execute AFTER_PULL if specified
+                if (!empty(AFTER_PULL)) {
+                    try {
+                        fputs($file, "*** AFTER_PULL INITIATED ***" . "\n");
+                        $result = shell_exec(AFTER_PULL);
+                        fputs($file, $result . "\n");
+                    } catch (Exception $e) {
+                        fputs($file, $e . "\n");
+                    }
+                }
+                fputs($file, "*** AUTO PULL COMPLETE ***" . "\n");
+            } catch (Exception $e) {
+                fputs($file, $e . "\n");
+            }
+        } else {
+            fputs($file, "=== ERROR: DIR is not a repository ===" . "\n");
+        }
+    } else{
+        fputs($file, "=== ERROR: Pushed branch does not match BRANCH ===\n");
+    }
+}
+fputs($file, "\n\n" . PHP_EOL);
+fclose($file);
